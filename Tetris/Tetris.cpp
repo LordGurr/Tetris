69fc94636c2a90925f8b1549308f1d38a1dff990
Gustav Juul <50996232+LordGurr@users.noreply.github.com>
2023-04-13 19:50:13 +0200

    Add project files.

---
+++ Tetris/Tetris.cpp
@@ -0,0 +1,272 @@
+﻿// Tetris.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int nScreenWidth = 80;			// Console Screen Size X (columns)
+int nScreenHeight = 30;			// Console Screen Size Y (rows)
+
+wstring tetromino[7];
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char* pField = nullptr;
+
+int Rotate(int px, int py, int r)
+{
+	switch (r % 4)
+	{
+	case 0: return py * 4 + px;			// 0 degrees
+	case 1: return 12 + py - (px * 4);	// 90 degrees
+	case 2: return 15 - (py * 4) - px;  // 180 degrees
+	case 3: return 3 - py + (px * 4);   // 270 degrees
+	}
+	return 0;
+}
+
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	for (int px = 0; px < 4; px++)
+	{
+		for (int py = 0; py < 4; py++)
+		{
+			//Index of piece
+			int pi = Rotate(px, py, nRotation);
+
+			//Index into field
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+				{
+					if (tetromino[nTetromino][pi] == L'x' && pField[fi] != 0)
+					{
+						return false;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	// Create Screen Buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	std::cout << "Hello World!\n";
+
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+
+	tetromino[1].append(L"..x.");
+	tetromino[1].append(L".xx.");
+	tetromino[1].append(L".x..");
+	tetromino[1].append(L"....");
+
+	tetromino[2].append(L".x..");
+	tetromino[2].append(L".xx.");
+	tetromino[2].append(L"..x.");
+	tetromino[2].append(L"....");
+
+	tetromino[3].append(L"....");
+	tetromino[3].append(L".xx.");
+	tetromino[3].append(L".xx.");
+	tetromino[3].append(L"....");
+
+	tetromino[4].append(L"..x.");
+	tetromino[4].append(L".xx.");
+	tetromino[4].append(L"..x.");
+	tetromino[4].append(L"....");
+
+	tetromino[5].append(L"..x.");
+	tetromino[5].append(L"..x.");
+	tetromino[5].append(L".xx.");
+	tetromino[5].append(L"....");
+
+	tetromino[6].append(L".x..");
+	tetromino[6].append(L".x..");
+	tetromino[6].append(L".xx.");
+	tetromino[6].append(L"....");
+
+	pField = new unsigned char[nFieldWidth * nFieldHeight]; // Play field buffer
+	for (int x = 0; x < nFieldWidth; x++) // Boundary
+	{
+		for (int y = 0; y < nFieldHeight; y++)
+		{
+			pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+		}
+	}
+	bool bGameOver = false;
+
+	int nCurrentPiece = 0;
+	int nCurrentRotation = 0;
+	int nCurrentX = nFieldWidth / 2;
+	int nCurrentY = 0;
+
+	bool bKey[4];
+	bool bRotateHold = false;
+
+	int nSpeed = 20;
+	int nSpeedCounter = 0;
+	bool bForceDown = false;
+	int nPieceCount = 0;
+	int nScore = 0;
+
+	vector<int> vLines;
+	while (!bGameOver)
+	{
+		// GAME TIMING ======================================
+		this_thread::sleep_for(50ms);
+		nSpeedCounter++;
+		bForceDown = (nSpeedCounter == nSpeed);
+
+		// INPUT ============================================
+		for (int k = 0; k < 4; k++)
+		{														// R    L   D Z
+			bKey[k] = (0x8000 & GetAsyncKeyState((unsigned char)("\x27\x25\x28Z"[k]))) != 0;
+		}
+		// UPDATE ===========================================
+		nCurrentX += (bKey[0] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY));
+		nCurrentX -= (bKey[1] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY));
+		nCurrentY += (bKey[2] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1));
+		nCurrentRotation += (!bRotateHold && bKey[3] && DoesPieceFit(nCurrentPiece, nCurrentRotation + 1, nCurrentX, nCurrentY));
+		bRotateHold = bKey[3];
+
+		if (bForceDown)
+		{
+			if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1))
+			{
+				nCurrentY++; // Fortsätt ner!
+			}
+			else
+			{
+				//Lock current piece
+				for (int px = 0; px < 4; px++)
+				{
+					for (int py = 0; py < 4; py++)
+					{
+						if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'x')
+						{
+							pField[(nCurrentY + py) * nFieldWidth + (nCurrentX + px)] = 10;
+						}
+					}
+				}
+				nPieceCount++;
+				if (nPieceCount % 10 == 0)
+				{
+					if (nSpeed >= 10)
+					{
+						nSpeed--;
+					}
+				}
+
+				//Full line
+				for (int py = 0; py < 4; py++)
+				{
+					if (nCurrentY + py < nFieldHeight - 1)
+					{
+						bool bLine = true;
+						for (int px = 1; px < nFieldWidth - 1; px++)
+						{
+							bLine &= (pField[(nCurrentY + py) * nFieldWidth + px]) != 0;
+						}
+
+						if (bLine)
+						{
+							for (int px = 1; px < nFieldWidth - 1; px++)
+							{
+								pField[(nCurrentY + py) * nFieldWidth + px] = 8;
+							}
+							vLines.push_back(nCurrentY + py);
+						}
+					}
+				}
+				nScore += 25;
+				if (!vLines.empty())nScore += (1 << vLines.size()) * 100;
+
+				//Choose next
+				nCurrentX = nFieldWidth / 2;
+				nCurrentY = 0;
+				nCurrentRotation = 0;
+				nCurrentPiece = rand() % 7;
+
+				bGameOver = !DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY);
+			}
+			nSpeedCounter = 0;
+		}
+		// DRAW =============================================
+
+		//Field
+		for (int x = 0; x < nFieldWidth; x++)
+		{
+			for (int y = 0; y < nFieldHeight; y++)
+			{
+				screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#█"[pField[y * nFieldWidth + x]];
+			}
+		}
+
+		// Draw Current Piece
+		for (int px = 0; px < 4; px++)
+		{
+			for (int py = 0; py < 4; py++)
+			{
+				wchar_t temp = tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)];
+				if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'x')
+				{
+					screen[(nCurrentY + py + 2) * nScreenWidth + (nCurrentX + px + 2)] = L'█';//nCurrentPiece + 65;
+				}
+			}
+		}
+
+		swprintf_s(&screen[2 * nScreenWidth + nFieldWidth + 6], 16, L"SCORE: %8d", nScore);
+
+		if (!vLines.empty())
+		{
+			WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+			this_thread::sleep_for(400ms);
+			for (auto& v : vLines)
+			{
+				for (int px = 1; px < nFieldWidth - 1; px++)
+				{
+					for (int py = vLines[0]; py > 0; py--)
+					{
+						pField[py * nFieldWidth + px] = pField[(py - 1) * nFieldWidth + px];
+					}
+					pField[px] = 0;
+					WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+				}
+			}
+			vLines.clear();
+		}
+
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
+	CloseHandle(hConsole);
+	std::cout << "Game Over! Score: " << nScore << endl;
+	system("pause");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started:
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
